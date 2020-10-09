b9c51a5c4b6c732bee03f880126eb46a18b84bf2
Nontawat Kanboon <nontawatkanboon@Nontawats-MacBook-Pro.local>
2020-10-09 18:31:32 +0700

    setup project

---
+++ EwsAppiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EwsAppiOS
+//
+//  Created by Nontawat Kanboon on 9/10/2563 BE.
+//  Copyright © 2563 ssoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
